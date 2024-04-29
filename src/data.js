7375523048b9341da75afd5ed215f580debcb12c
richellepintucan <richellepintucan@yahoo.com>
2024-04-29 19:00:59 +0200

    edited images path

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ const data = {
             title: 'Mussels with fries',
             category: 'dinner',
             price: 22.99,
-            src: '../assets/item-0.jpg',
+            src: './assets/item-0.jpg',
             desc: `Served piping hot and bursting with flavor, our succulent mussels are cooked to perfection in a savory broth infused with aromatic herbs and spices. Each plump mussel is a burst of ocean freshness, delicately balanced with the richness of our homemade sauce.`,
         },
         {
@@ -13,7 +13,7 @@ const data = {
             title: 'Buttermilk pancakes',
             category: 'breakfast',
             price: 12.99,
-            src: '../assets/item-1.jpg',
+            src: './assets/item-1.jpg',
             desc: `Perfect, golden pancakes. Carefully perfected to yield fat, fluffy, and super flavorful pancakes (the cousin to paper-thin crepes)`,
         },
         {
@@ -21,7 +21,7 @@ const data = {
             title: 'Chicken fried noodles',
             category: 'lunch',
             price: 19.99,
-            src: '../assets/item-2.jpg',
+            src: './assets/item-2.jpg',
             desc: `Whether you're craving a quick lunch or a satisfying dinner, our Chicken Fried Noodles are the perfect choice for food lovers everywhere. Treat yourself to a taste sensation today!`,
         },
         {
@@ -29,7 +29,7 @@ const data = {
             title: 'Refreshing milkshake',
             category: 'shakes',
             price: 8.99,
-            src: '../assets/item-3.jpg',
+            src: './assets/item-3.jpg',
             desc: `Quench your thirst and indulge your taste buds with our Refreshing Milkshake, the ultimate blend of creamy goodness and irresistible flavors. Crafted to perfection, each sip is a symphony of delight that will leave you feeling refreshed and rejuvenated.`,
         },
         {
@@ -37,7 +37,7 @@ const data = {
             title: 'Combo brekky',
             category: 'breakfast',
             price: 14.99,
-            src: '../assets/item-4.jpg',
+            src: './assets/item-4.jpg',
             desc: `Combo Breakfast: The Perfect Start to Your Day! Rise and shine with our hearty Combo Breakfast, a delicious assortment of morning favorites designed to fuel your day with energy and flavor. Whether you're craving a savory feast or a sweet indulgence, our combo has something to satisfy every appetite.`,
         },
         {
@@ -45,7 +45,7 @@ const data = {
             title: 'Whole-some pizza',
             category: 'lunch',
             price: 22.99,
-            src: '../assets/item-5.jpg',
+            src: './assets/item-5.jpg',
             desc: `Pizza: A Slice of Happiness! Embark on a culinary journey to Italy without leaving your doorstep with our mouthwatering pizza! Crafted with love and precision, each slice is a perfect blend of flavors, textures, and aromas that will leave you craving more.`,
         },
         {
@@ -53,7 +53,7 @@ const data = {
             title: 'Oreo milkshake',
             category: 'shakes',
             price: 9.99,
-            src: '../assets/item-6.jpg',
+            src: './assets/item-6.jpg',
             desc: `Dive into a world of creamy delight with our irresistible Oreo Milkshake, where classic cookies meet luscious ice cream for a flavor sensation that will leave you craving more. Crafted with care and packed with Oreo goodness, each sip is a heavenly experience that's sure to satisfy your sweet tooth.`,
         },
         {
@@ -61,7 +61,7 @@ const data = {
             title: 'Sushi Delight',
             category: 'dinner',
             price: 24.99,
-            src: '../assets/item-7.jpeg',
+            src: './assets/item-7.jpeg',
             desc: `Embark on a culinary journey to Japan with our exquisite Sushi Delight! Experience the artistry and precision of our skilled sushi chefs as they craft each roll with the freshest ingredients and expert technique. From traditional favorites like California Rolls and Spicy Tuna Rolls to innovative creations bursting with flavor, there's something for every palate to enjoy.`,
         },
         {
@@ -69,7 +69,7 @@ const data = {
             title: 'Sunny breakfast',
             category: 'breakfast',
             price: 12.99,
-            src: '../assets/item-8.jpg',
+            src: './assets/item-8.jpg',
             desc: `Start your day on the sunny side with our delectable Sunny Side Up Egg Breakfast, a classic morning favorite that's sure to brighten your morning and satisfy your hunger. Featuring perfectly cooked eggs, crispy bacon, and all the fixings, it's the ultimate way to kickstart your day with flavor and flair.`,
         },
         {
@@ -77,7 +77,7 @@ const data = {
             title: 'Lunch bites',
             category: 'lunch',
             price: 14.99,
-            src: '../assets/item-9.jpg',
+            src: './assets/item-9.jpg',
             desc: `A Flavorful Feast on the Go! Satisfy your hunger and tantalize your taste buds with our mouthwatering Lunch Bites Sandwich, the perfect blend of savory fillings, fresh ingredients, and artisan bread that's guaranteed to delight with every bite. Whether you're on the move or taking a moment to savor, our sandwich is the ultimate lunchtime treat that's both convenient and delicious.`,
         },
         {
@@ -85,7 +85,7 @@ const data = {
             title: 'Vanilla shake',
             category: 'shakes',
             price: 8.99,
-            src: '../assets/item-10.jpg',
+            src: './assets/item-10.jpg',
             desc: `Take a sip into nostalgia with our timeless Vanilla Shake, a creamy concoction that brings back memories of childhood treats and simple pleasures. Crafted with premium vanilla ice cream and a touch of magic, each sip is a journey into sweetness and satisfaction`,
         },
         {
@@ -93,7 +93,7 @@ const data = {
             title: 'Flame-grilled steak',
             category: 'dinner',
             price: 22.99,
-            src: '../assets/item-11.jpg',
+            src: './assets/item-11.jpg',
             desc: `Savor the mouthwatering taste of our flame-grilled steak, expertly crafted to perfection over an open flame for an unforgettable dining experience. From the first sear to the final juicy bite, each steak is a testament to our commitment to quality, flavor, and satisfaction.`,
         },
     ],
